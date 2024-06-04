99c6387508bae457a2229fc12caa7be0d0804432
Amardeep Sadanande <155010545+Amardeep2357@users.noreply.github.com>
2024-06-04 19:33:25 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from OTS.views import *
+app_name='OTS'
+urlpatterns = [
+    path('', welcome),
+    path('new-Candidate',candidateRegistrationForm ,name='registrationForm'),
+    path('store-Candidate',candidateRegistration, name='storeCandidate'),
+    path('login',loginView ,name='login'),
+    path('home',candidateHome ,name='home'),
+    path('test-paper',testpaper, name='testpaper'),
+    path('calculate-result',calculateTestResult, name='calculateTest'),
+    path('test-history',testResultHistory,name='testHistory'),
+    path('result',showTestResult, name='result'),
+    path('logout',logoutView,name='logout'),
+   
+
+]
